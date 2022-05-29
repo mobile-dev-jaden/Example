30c8c14c3b7b974d39144604ddf0357edbcfa989
Taeho-Kim <taehokim@hicare.net>
2022-05-29 12:49:02 +0900

    Initial Commit

---
+++ Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by 김태호 on 2022/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by 김태호 on 2022/05/29.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
